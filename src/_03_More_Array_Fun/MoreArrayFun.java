09c0afb3504bae249cb9bdb660a455414944eed4
Oliver Nizet <olivernizet@att.net>
2018-08-30 19:29:53 -0700

    a

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,12 +5,20 @@ import java.util.Random;
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		
+		String[] pa = new String[3];
+		pa[0] = "one";
+		pa[1] = "two";
+		pa[2] = "three";
+		printAll(pa);
 	}
 	
 	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	static //2. Write a method that takes an array of Strings and prints all the Strings in the array.
+	void printAll(String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
