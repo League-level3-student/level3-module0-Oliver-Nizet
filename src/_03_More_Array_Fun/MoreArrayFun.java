334233071d2fb34d36e2ff42d90c5d6d843ec81c
Oliver Nizet <36498022+grace04@users.noreply.github.com>
2018-09-04 17:39:45 -0700

    done

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -3,34 +3,46 @@ package _03_More_Array_Fun;
 import java.util.Random;
 
 public class MoreArrayFun {
-	//1. Create a main method to test the other methods you write.
+	// 1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		String[] pa = new String[3];
+		String[] pa = new String[5];
 		pa[0] = "one";
 		pa[1] = "two";
 		pa[2] = "three";
+		pa[3] = "four";
+		pa[4] = "five";
 		printAll(pa);
+		printAllReverse(pa);
+		printEO(pa);
 	}
-	
-	
-	static //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	void printAll(String[] arr) {
+
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	static void printAll(String[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 	}
-	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	static void printAllReverse(String[] arr) {
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	static void printEO(String[] arr) {
+		for (int i = 0; i < arr.length; i += 2) {
+			System.out.println(arr[i]);
+		}
+	}
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+
 }
